1425a48243d25aeca4831beebe729471d5112f13
sponrad <sponrad@gmail.com>
2016-08-06 20:21:33 -0700

    normal and fire type effects

--- Assets/Scripts/test02Scripts/l02ProjectileScript.cs
+++ Assets/Scripts/test02Scripts/l02ProjectileScript.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class l02ProjectileScript : MonoBehaviour {
 
 	public float speed;
+	public enum Type {lightning, water, ice, normal, fire};
+	public Type type;
+
+	public bool splitOnMatch = false;
+	public bool explodeOnMatch = false;
+
+	private bool preventDestroy = false;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
@@ -19,8 +25,42 @@ public class l02ProjectileScript : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D coll){
 		if (coll.gameObject.tag == "enemy") {
 
-			Destroy (coll.gameObject);
-			Destroy (gameObject);
+			if (coll.gameObject.GetComponent<l02MonsterScript> ().type.ToString () == type.ToString ()) {
+				if (explodeOnMatch) {
+					explodeProjectile ();
+					Destroy (coll.gameObject);
+				}
+
+				if (splitOnMatch) {
+					splitProjectile ();
+					Destroy (coll.gameObject);
+					Destroy (gameObject);
+				}
+			} else {
+				Destroy (coll.gameObject);
+				if (!preventDestroy) {
+					Destroy (gameObject);
+				}
+			}
 		}
+	}
+
+	void explodeProjectile(){
+		//take out the surrounding objects.
+		Debug.Log("FIRE");
+		transform.localScale = new Vector3(5f, 5f, 5f);
+		speed = 0f;
+		preventDestroy = true;
+		Invoke ("destroy", .3f);
+	}
+
+	void splitProjectile(){
+		//create two projectiles of this same type behind it.. going diagonally
+		Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z), Quaternion.Euler( new Vector3(0f, 0f, 45f)));
+		Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z), Quaternion.Euler( new Vector3(0f, 0f, -45f)));
+	}
+
+	void destroy(){
+		Destroy (gameObject);
 	}
 }
