b8fa2b18638b6a6a5c2abb1df570ed1794ee55f6
sponrad <sponrad@gmail.com>
2016-09-07 12:49:26 -0700

    empty tower

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -8,7 +8,7 @@ public class Globals : MonoBehaviour
 	public static int gold = 50;
 	public static string levelScores;
 
-	public static List<string> towersInPlay = new List<string> {"arrow01", "arrow01", "arrow01", "arrow01", "arrow01"};
+	public static List<string> towersInPlay = new List<string> {"empty", "arrow01", "arrow01", "arrow01", "arrow01"};
 	//TODO: figure out if i need the owned list
 	public static List<string> towersOwned = new List<string> {"arrow01", "arrow01", "arrow01", "arrow01", "arrow01", "fire01", "lightning01"}; 
 	public static List<string> towerInventory = new List<string> {"lightning01", "fire01"};
